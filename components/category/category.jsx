a2f327ad7fbf1f8f61f1fe19b8f2ef6cfd492284
realmann <sappiah085@st.ug.edu.gh>
2023-02-19 14:07:17 +0100

    changes

--- components/category/category.jsx
+++ components/category/category.jsx
@@ -1,7 +1,7 @@
 import MovieCard from '../movieCard/movieCard';
 export default function Category({ label }) {
   return (
-    <div className={' flex-col gap-2 w-full hidden md:flex'}>
+    <div className={' flex-col gap-2 w-full  md:flex'}>
       <h2 className={`md:pl-14 text-zinc-300 text-2xl font-medium`}>{label}</h2>
       <div className="flex scrollbar-hide w-full gap-[.1rem] relative flex-nowrap overflow-x-scroll">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((el) => (
